1b75f22460aaa7d35fbf2b501e371d5d05ee13d3
iMADi-ARCH <nandanaditya985@gmail.com>
2021-05-20 15:02:19 +0530

    Testing

---
+++ main.py
@@ -0,0 +1,13 @@
+from kivymd.app import MDApp
+from kivy.uix.widget import Widget
+from kivymd.uix.button import MDRectangleFlatButton
+from kivymd.uix.button import MDRaisedButton
+from kivymd.uix.gridlayout import MDGridLayout
+
+class BeatWorkoutApp(MDApp):
+    pass
+
+class MainWidget(MDGridLayout):
+    pass
+
+BeatWorkoutApp().run()
